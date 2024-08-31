cc96a58fa9a0a6ba3c76c9c5367311bb1ee74efa
Vaisakh <vaisakhpn78@gmail.com>
2024-08-31 15:29:33 +0530

    Custom button added

---
+++ client/components/ui/Button/Button.jsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { AVAILABLE_THEMES, DEFAULT_BUTTON_COLOR } from "./constant";
+
+const Button = ({ label, buttonColor = "primary", disabled, ...restProps }) => {
+  return (
+    <button
+      disabled={disabled}
+      type="submit"
+      className={`disabled:opacity-70 disabled:cursor-not-allowed p-2 text-16 text-white rounded-3xl hover:opacity-80 transition w-4/12 flex items-center justify-center gap-2  ${
+        AVAILABLE_THEMES[buttonColor] || DEFAULT_BUTTON_COLOR
+      }`}
+      {...restProps}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
---
+++ client/components/ui/Button/constant.js
@@ -0,0 +1,5 @@
+export const DEFAULT_BUTTON_COLOR = "bg-blue-600";
+export const AVAILABLE_THEMES = {
+  primary: DEFAULT_BUTTON_COLOR,
+  secondary: "bg-red-600",
+};
