b9eb438c23c5561e36bf0c4050b8fc145958c565
Vaisakh <vaisakhpn78@gmail.com>
2024-08-31 22:39:20 +0530

    modified Providers of redux

--- client/redux/Providers.jsx
+++ client/redux/Providers.jsx
@@ -1,10 +1,20 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store";
 
 function ReduxProvider({ children }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // or some loading spinner
+  }
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
