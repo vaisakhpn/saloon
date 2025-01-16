f4a8e7e2510c76b2bf3fb58c4e7ca96d24bd9976
Vaisakh <vaisakhpn78@gmail.com>
2025-01-16 20:49:20 +0530

    Spelling correction

--- client/components/ui/slider/SliderData.js
+++ client/components/ui/slider/SliderData.js
@@ -1,9 +1,9 @@
 export const sliderData = [
   {
     id: 1,
-    title: "Book Your Perfect Saloon Experience",
+    title: "Book Your Perfect Salon Experience",
     tagline:
-      "Discover the best saloons near you and book appointments online with ease",
+      "Discover the best salons near you and book appointments online with ease",
     image: "/assets/hero.jpg",
     buttons: [{ id: 1, text: "Book Now", link: "/Booking", type: "primary" }],
   },
